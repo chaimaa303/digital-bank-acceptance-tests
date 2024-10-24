40d1a82c2526670c0be7b52c462a8937c10cab40
Chaimaa <denize.ele@gmail.com>
2024-10-24 09:17:46 -0400

    change runners class name

--- src/test/java/co/wedevx/digitalbank/automation/ui/UiRegressionRunner/UiRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/UiRegressionRunner/UiRegressionRunner.java
@@ -1,4 +1,4 @@
-package co.wedevx.digitalbank.automation.ui.runners;
+package co.wedevx.digitalbank.automation.ui.UiRegressionRunner;
 
 
 import org.junit.platform.suite.api.*;
@@ -10,6 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("ui/features/LoginAndRegistration")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value="co.wedevx.digitalbank.automation.ui.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, junit:target/cucumberReports/junit.xml, json:target/cucumberReports/report.json, html:target/cucumberReports/report.html")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/CucumberReports/report.json")
 @IncludeTags("Test")
  public class UiRegressionRunner {
     // No additional code needed here
