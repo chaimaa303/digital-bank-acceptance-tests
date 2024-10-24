dbf2c97e2e9f76341c5adf06c157de6007b1ba8e
Chaimaa <denize.ele@gmail.com>
2024-10-24 08:09:07 -0400

    modif after changing pom file and add hibernate

--- src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("ui/features/checking")
+@SelectClasspathResource("ui/features/LoginAndRegistration")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value="co.wedevx.digitalbank.automation.ui.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, junit:target/cucumberReports/junit.xml, json:target/cucumberReports/report.json, html:target/cucumberReports/report.html")
 @IncludeTags("Test")
